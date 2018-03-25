c2df0c1d853761d8ff47ccdef21f3dedae2ca9a2
Hugo Peters <hpeters1024@gmail.com>
2018-03-25 22:15:00 +0200

    Now drop the css as a new article elaborate how AJAX calls work

--- pages/js/ajax.js
+++ pages/js/ajax.js
@@ -1,6 +1,11 @@
 function build() {
     var req = new XMLHttpRequest();
-    req.addEventListener("loadend", function() {alert(this.responseText);});
+    req.addEventListener("loadend", function() { 
+        var page= document.getElementById("page");
+        var item = document.createElement("article");
+        item.innerHTML = this.responseText;
+        page.appendChild(item);
+    })
     req.open("GET", "css/main.css", true);
     req.send(null);
 }
