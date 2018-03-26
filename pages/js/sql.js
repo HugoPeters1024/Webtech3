2de2fab7c78d1bfabef4816ca3c410f2e97fb2ec
Robin Sikkens <github@robinsikkens.nl>
2018-03-26 14:16:55 +0200

    sql.js

--- pages/js/sql.js
+++ pages/js/sql.js
@@ -1,29 +1,27 @@
-var sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose();
 
-function testConnect(){
-  var db = new sqlite3.Database('db/test.db', (err) => {
-    if (err) {
-      return console.error('There was an error connecting to the database');
-    }
-    console.log('Database connection has been established.');
-  });
+let db = new sqlite3.Database('db/test.db', (err) => {
+  if (err) {
+    return console.error('There was an error connecting to the database');
+  }
+  console.log('Database connection has been established.');
+});
 
-  db.serialize(() => {
-    db.each(`CREATE TABLE test_table (
-                name string,
-                age integer,
-              );`, (err, row) => {
-        if (err) {
-          console.error(err.message);
-        }
-        console.log('Created Table');
-      });
-  });
+db.serialize(() => {
+  db.each(`CREATE TABLE test_table (
+              name string,
+              age integer,
+            );`, (err, row) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log('Created Table');
+    });
+});
 
-  db.close((err) => {
-    if (err) {
-      return console.error('There was an error closing the database connection');
-    }
-    console.log('Database connection has been closed');
-  });
-}
+db.close((err) => {
+  if (err) {
+    return console.error('There was an error closing the database connection');
+  }
+  console.log('Database connection has been closed');
+});
