102d40d49b3438838eed45adfcec43a9bfa36902
Hugo Peters <hpeters1024@gmail.com>
2018-03-30 17:43:35 +0200

    DROP Problems

--- pages/js/ajax.js
+++ pages/js/ajax.js
@@ -16,7 +16,11 @@ function build(file) {
 function test() {
     var req = new XMLHttpRequest();
     req.addEventListener("loadend", function() { alert(this.responseText) });
-    req.open("POST", "post", true);
+    req.open("POST", "http://www.webtech.science.uu.nl/group12/post", true);
     req.setRequestHeader("Content-Type", "application/json");
-    req.send(`{"method": "INSERT"}`); 
+   // req.setRequestHeader("Access-Control-Allow-Origin", "*");
+    //req.setRequestHeader("Access-Control-Allow-Methods", "GET, POST");
+    //req.setRequestHeader("Allow", "GET POST");
+    //req.setRequestHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+    req.send(`{"method": "SELECT", "table" : "Users"}`); 
 }
