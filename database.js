d92178a853bc2538ab7d79ac9f812ec3aace9bce
Hugo Peters <hpeters1024@gmail.com>
2018-04-05 21:22:34 +0200

    strings

--- database.js
+++ database.js
@@ -86,7 +86,7 @@ exports.dbRegister = (req, res) => {
    var last_name = xss(req.body.last_name);
    var address = xss(req.body.address);
    var email = xss(req.body.email);
-   var salt = sha256(Math.random());
+   var salt = sha256(Math.random()).toString();
    var password = sha256(req.body.password + salt);
    var db = openDB();
    db.run("INSERT INTO Users (username, first_name, last_name, address, email, password, salt) VALUES ('"+username+"',' "+first_name+"','"+last_name+"','"+address+"', '"+email+"', '"+password+"', '"+salt+"')", err => {
