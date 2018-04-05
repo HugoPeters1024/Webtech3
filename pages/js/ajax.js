6fa3b54601b1108302fefa706d0fde8b2da50839
Hugo Peters <hpeters1024@gmail.com>
2018-04-05 23:23:30 +0200

    mehh

--- pages/js/ajax.js
+++ pages/js/ajax.js
@@ -77,7 +77,7 @@ function buildProductPage()
             var row = document.createElement("tr");
             row.innerHTML = product.GetHtml();
             table.appendChild(row);
-            var but = document.getElementById(`product_button${product.product_id}`);
+            var but = row.getElementById(`product_button${product.product_id}`);
             alert(`product_button${product.product_id}`);
             but.addEventListener("click", function() { build("confirm_product.html", buildProductConfirmPage, JSON.stringify(product)) });
         }
