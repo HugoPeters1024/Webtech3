d98e64726de1acb3bdff21baa06b58785a3c7d99
Hugo Peters <hpeters1024@gmail.com>
2018-04-05 19:48:33 +0200

    yeah

--- pages/js/ajax.js
+++ pages/js/ajax.js
@@ -93,8 +93,8 @@ function buildHistoryPage() {
             alert("Invalid server response, not a JSON object")
         }
         if (response.err) {
+            console.log(response.err);
             if (response.errcode == "32") { //Token no longer valid. 
-                alert(response.err);
                 SetState("LoggedIn", false);
                 build("login.html");
             }
