cfa0df97ce448995913a5b6d190ccc5a3467cd35
Hugo Peters <hpeters1024@gmail.com>
2018-04-09 20:38:13 +0200

    wouter is gay

--- pages/js/ajax.js
+++ pages/js/ajax.js
@@ -236,7 +236,7 @@ function buildProductPage(maker_id, order_id, search_text, limit)
             };
 
             cat_list = document.getElementById("cat_list");
-            for(cat in categories) {
+            for(var cat in categories) {
                 alert(JSON.stringify(cat));
                 cat_list.appendChild(cat_list[cat].GetTree());
             };
