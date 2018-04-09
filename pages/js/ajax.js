f1bbac6bfc13dbd7cfc2d0d352b55b789cb91b87
Hugo Peters <hpeters1024@gmail.com>
2018-04-09 21:57:51 +0200

    wouter is gay

--- pages/js/ajax.js
+++ pages/js/ajax.js
@@ -238,7 +238,7 @@ function buildProductPage(maker_id, order_id, search_text, limit)
                 } else {
                     categories[categorie.cat_id] = categorie;
                 }
-                alert(cats);
+                alert(JSON.parse(cats));
                 cats.forEach(p => {
                     if (p.cat_id == categorie.cat_id)
                         categorie.items.push(new Object( { "null" : "null" }));
