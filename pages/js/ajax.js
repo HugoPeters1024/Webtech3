36405188d56e095be7581c21ee3ec75c08113c0f
Hugo Peters <hpeters1024@gmail.com>
2018-04-09 22:18:27 +0200

    wouter is gay

--- pages/js/ajax.js
+++ pages/js/ajax.js
@@ -246,7 +246,7 @@ function buildProductPage(maker_id, order_id, search_text, limit)
 
 
             cat_list = document.createElement("UL");
-            cat_list.setAttribute("class", "cat_list");
+            cat_list.setAttribute("class", "cat_list_sub");
             document.getElementById("side").appendChild(cat_list);
             for(var cat in categories) {
                 var b = categories[cat].GetTree();
